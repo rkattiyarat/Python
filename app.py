fb77a09c48ddd0a0f9734725de5055ba21eed127
Rungnapa <aom_bnn@hotmail.com>
2021-04-20 12:33:46 -0400

    started learning

---
+++ app.py
@@ -0,0 +1,27 @@
+
+def first():
+    age = 20
+    status = "new"
+    first_name = "John Smith"
+    print (first_name)
+    print (age)
+    print (status)
+
+
+
+
+def second():
+    name = input("what is your name? ")
+    print ("Hello " + name)
+
+def third():
+    birth_year = input("Enter your birth year: ")
+    age = 2021 - int(birth_year)
+    print(age)
+def fourth():
+    age = input("Enter your age:")
+    birth_year = (2021 - int (age))
+    print (birth_year)
+fourth()
+
+
